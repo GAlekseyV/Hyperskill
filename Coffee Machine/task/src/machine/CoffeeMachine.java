49f9af5e454af6f2707f74699b8494ac62402e50
Aleksey Guzenkov <galekseyv@gmail.com>
2019-05-11 17:16:49 +0300

    implement stage 5

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,59 +4,74 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    private static int hasWater = 1200;
+    private static int hasWater = 400;
     private static int hasMilk = 540;
     private static int hasCoffeeBeans = 120;
     private static int hasDisposableCup = 9;
     private static int hasMoney = 550;
 
+    private static int waterInEspresso = 250;
+    private static int beansInEspresso = 16;
+    private static int costEspresso = 4;
+
+    private static int waterInCappuccino = 200;
+    private static int milkInCappuccino = 100;
+    private static int beansInCappuccino = 12;
+    private static int costCappuccino = 6;
+
+    private static int waterInLatte = 350;
+    private static int milkInLatte = 75;
+    private static int beansInLatte = 20;
+    private static int costLatte = 7;
+
     private static Scanner scanner;
 
 
     public static void main(String[] args) {
+        boolean isContinue = true;
 
-        printSupplies();
-        System.out.println("Write action (buy, fill, take): ");
+        while(isContinue){
+            System.out.print("Write action (buy, fill, take, remaining, exit): ");
 
-        scanner = new Scanner(System.in);
-        String action = scanner.next();
+            scanner = new Scanner(System.in);
+            String action = scanner.next();
 
-        switch (action){
-            case "buy":
-                System.out.println("What do you want to buy?" +
-                        "1 - espresso, 2 - latte, 3 - cappuccino: ");
-                int wantedCoffee = scanner.nextInt();
-                if(wantedCoffee == 1){
-                    makeEspresso();
+            switch (action){
+                case "buy":
+                    System.out.print("What do you want to buy? " +
+                            "1 - espresso, 2 - latte, 3 - cappuccino, " +
+                            "back - to main menu: ");
+                    if(scanner.hasNextInt()){
+                        int wantedCoffee = scanner.nextInt();
+                        if(checkSupplies(wantedCoffee)){
+                            makeCoffee(wantedCoffee);
+                            System.out.println("I have enough resources, making you a coffee!");
+                        }else{
+                            System.out.println("Sorry, not enough water!");
+                        }
+                    }
+                    break;
+                case "fill":
+                    fillCoffeeMachine();
+                    break;
+                case "remaining":
                     printSupplies();
-                }else if(wantedCoffee == 2){
-                    makeLatte();
-                    printSupplies();
-                }else if(wantedCoffee == 3){
-                    makeCappuccino();
-                    printSupplies();
-                }
-                break;
-            case "fill":
-                fillCoffeeMachine();
-                printSupplies();
-                break;
-            case "take":
-                System.out.println("I gave you $" + getMoney());
-                takeMoney();
-                printSupplies();
-                break;
-            default:
-                System.out.println("Unknown action");
+                    break;
+                case "take":
+                    System.out.println("I gave you $" + getMoney());
+                    takeMoney();
+                    break;
+                case "exit":
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("Unknown action");
 
+            }
         }
-
     }
 
     private static void makeEspresso(){
-        int waterInEspresso = 250;
-        int beansInEspresso = 16;
-        int costEspresso = 4;
 
         hasWater -= waterInEspresso;
         hasCoffeeBeans -= beansInEspresso;
@@ -65,10 +80,6 @@ public class CoffeeMachine {
     }
 
     private static void makeLatte(){
-        int waterInLatte = 350;
-        int milkInLatte = 75;
-        int beansInLatte = 20;
-        int costLatte = 7;
 
         hasWater -= waterInLatte;
         hasMilk -= milkInLatte;
@@ -78,10 +89,6 @@ public class CoffeeMachine {
     }
 
     private static void makeCappuccino(){
-        int waterInCappuccino = 200;
-        int milkInCappuccino = 100;
-        int beansInCappuccino = 12;
-        int costCappuccino = 6;
 
         hasWater -= waterInCappuccino;
         hasMilk -= milkInCappuccino;
@@ -90,15 +97,31 @@ public class CoffeeMachine {
         hasDisposableCup--;
     }
 
+    private static void makeCoffee(int typeOfCoffee) {
+        switch (typeOfCoffee){
+            case 1:
+                makeEspresso();
+                break;
+            case 2:
+                makeLatte();
+                break;
+            case 3:
+                makeCappuccino();
+                break;
+            default:
+                System.out.println("Unknown coffee");
+        }
+    }
+
     private static void fillCoffeeMachine(){
-        System.out.println("Write how many ml of water do you want to add: ");
+        System.out.print("Write how many ml of water do you want to add: ");
         hasWater += scanner.nextInt();
-        System.out.println("Write how many ml of milk do you want to add: ");
+        System.out.print("Write how many ml of milk do you want to add: ");
         hasMilk += scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans do you" +
+        System.out.print("Write how many grams of coffee beans do you" +
                 " you want to add: ");
         hasCoffeeBeans += scanner.nextInt();
-        System.out.println("Write how many disposable cups of coffee do you " +
+        System.out.print("Write how many disposable cups of coffee do you " +
                 "want to add: ");
         hasDisposableCup += scanner.nextInt();
     }
@@ -117,7 +140,41 @@ public class CoffeeMachine {
         System.out.println(hasMilk + " of milk");
         System.out.println(hasCoffeeBeans + " of coffee beans");
         System.out.println(hasDisposableCup + " of disposable cups");
-        System.out.println(hasMoney + " of money");
+        System.out.println("$" + hasMoney + " of money");
         System.out.println();
+    }
+
+    private static boolean checkSupplies(int typeOfCoffee){
+        switch (typeOfCoffee){
+            case 1:
+                return checkSuppliesForEspresso();
+            case 2:
+                return checkSuppliesForCappuccino();
+            case 3:
+                return checkSuppliesForLatte();
+            default:
+                System.out.println("Unknown coffee");
+        }
+        return false;
+    }
+
+    private static boolean checkSuppliesForEspresso() {
+        return hasWater >= waterInEspresso
+                && hasCoffeeBeans >= beansInEspresso
+                && hasDisposableCup > 0;
+    }
+
+    private static boolean checkSuppliesForCappuccino() {
+        return hasWater >= waterInCappuccino
+        && hasMilk >= milkInCappuccino
+        && hasCoffeeBeans >= beansInCappuccino
+        && hasDisposableCup > 0;
+    }
+
+    private static boolean checkSuppliesForLatte() {
+        return hasWater >= waterInLatte
+        && hasMilk >= waterInLatte
+        && hasCoffeeBeans >= beansInLatte
+        && hasDisposableCup > 0;
     }
 }
